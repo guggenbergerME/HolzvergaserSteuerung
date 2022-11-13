cb99ed0d6251fc246ce85c62db736949ec22930c
TGuggenberger_peaq <github@guggeberger.me>
2022-11-13 20:43:01 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,118 +2,107 @@
 #include <SPI.h>
 #include <ESP8266WiFi.h> 
 #include <PubSubClient.h>   
+#include <PCF8574.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-const byte relais_A = D0;       
-const byte relais_B = D5;      
-const byte relais_C = D6;       
-const byte relais_D = D7; 
+/////////////////////////////////////////////////////////////////////////// Funktionsprototypen
+void loop                       ();
+void wifi_setup                 ();
+void callback                   (char* topic, byte* payload, unsigned int length);
+void reconnect                  ();
+void temp_messen                ();
+void mqtt_callback_aufrufen     ();
 
-// Kartendaten
+//////////////////////////////////////////////////////////////////////////// PCF8574 Adresse
+PCF8574 pcf8574(0x20);
+// Wemso SDA und SDC Pins
 
-const char* kartenID = "Relaiskarte_0003";
+/////////////////////////////////////////////////////////////////////////// Schleifen verwalten
+unsigned long previousMillis_temp_messen = 0; // Spannung Messen
+unsigned long interval_temp_messen = 10000; 
+
+unsigned long previousMillis_mqtt_callback = 0; // Spannung Messen
+unsigned long interval_mqtt_callback = 1000; 
+
+/////////////////////////////////////////////////////////////////////////// mqtt variable
+char msgToPublish[60];
+char stgFromFloat[10];
+char textTOtopic[60];
+const char* kartenID = "HolzvergaserSteuerung";
+
+/////////////////////////////////////////////////////////////////////////// WIRE Bus
+#define ONE_WIRE_BUS D5
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+/////////////////////////////////////////////////////////////////////////// Temp. Sensor HEX zuweisen
+DeviceAddress temp_abgas         = { 0x28, 0xAA, 0x6C, 0x90, 0x16, 0x13, 0x02, 0xC2 }; 
+const char* topic_temp_abgas     = "Heizung/Holzvergaser/temp/abgas";
+
+DeviceAddress temp_kessel         = { 0x28, 0xAA, 0x6C, 0x90, 0x16, 0x13, 0x02, 0xC2 }; 
+const char* topic_temp_kessel     = "Heizung/Holzvergaser/temp/kessel";
+
+DeviceAddress temp_rl            = { 0x28, 0xAA, 0x6C, 0x90, 0x16, 0x13, 0x02, 0xC2 }; 
+const char* topic_temp_rl        = "Heizung/Holzvergaser/temp/ruecklauf";
+
+DeviceAddress temp_vl            = { 0x28, 0xAA, 0x6C, 0x90, 0x16, 0x13, 0x02, 0xC2 }; 
+const char* topic_temp_vl        = "Heizung/Holzvergaser/temp/vorlauf";
+
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// Connect to the WiFi
+/////////////////////////////////////////////////////////////////////////// wifi daten
 const char* ssid = "GuggenbergerLinux";
 const char* password = "Isabelle2014samira";
 const char* mqtt_server = "192.168.150.1";
-//const char* mqtt_topic_a = "192.168.178.222";
 
-
+/////////////////////////////////////////////////////////////////////////// Callback
 void callback(char* topic, byte* payload, unsigned int length) {
-/*
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
 
-  Serial.println();
-*/
-
-
-    if (strcmp(topic,"Relaiskarte_0003/IN/A")==0) {
+    if (strcmp(topic,"Heizung/Holzvergaser/geblaese/IN")==0) {
 
         // Kanal A
         if ((char)payload[0] == 'o' && (char)payload[1] == 'n') {  
-                 Serial.println("relais_A -> AN");
-                 digitalWrite(relais_A, !HIGH);
-                 client.publish("Relaiskarte_0003/OUT/A","on");
+                 Serial.println("Abgas Gebläse AN");
+                 //digitalWrite(relais_A, !HIGH);
+                 //client.publish("Relaiskarte_0003/OUT/A","on");
                 delay(100);
               }
 
         if ((char)payload[0] == 'o' && (char)payload[1] == 'f' && (char)payload[2] == 'f') {  
-                 Serial.println("relais_A -> AUS");
-                 digitalWrite(relais_A, !LOW);
-                 client.publish("Relaiskarte_0003/OUT/A","off");
+                 Serial.println("Abgas Gebläse AUS");
+                 //digitalWrite(relais_A, !LOW);
+                 //client.publish("Relaiskarte_0003/OUT/A","off");
+                delay(100);
+              }
+      } 
+ 
+    if (strcmp(topic,"Heizung/Holzvergaser/pumpe/heizkreis/IN")==0) {
+
+        // Kanal A
+        if ((char)payload[0] == 'o' && (char)payload[1] == 'n') {  
+                 Serial.println("Pumpe Heizkreis AN");
+                 //digitalWrite(relais_A, !HIGH);
+                 //client.publish("Relaiskarte_0003/OUT/A","on");
+                delay(100);
+              }
+
+        if ((char)payload[0] == 'o' && (char)payload[1] == 'f' && (char)payload[2] == 'f') {  
+                 Serial.println("Pumpe Heizkreis AUS");
+                 //digitalWrite(relais_A, !LOW);
+                 //client.publish("Relaiskarte_0003/OUT/A","off");
                 delay(100);
               }
       } 
 
 
 
-    if (strcmp(topic,"Relaiskarte_0003/IN/B")==0) {
-
-        // Kanal B
-        if ((char)payload[0] == 'o' && (char)payload[1] == 'n') {  
-                 Serial.println("relais_B -> AN");
-                 digitalWrite(relais_B, !HIGH);
-                 client.publish("Relaiskarte_0003/OUT/B","on");                 
-                delay(100);
-              }
-
-        if ((char)payload[0] == 'o' && (char)payload[1] == 'f' && (char)payload[2] == 'f') {  
-                 Serial.println("relais_B -> AUS");
-                 digitalWrite(relais_B, !LOW);
-                 client.publish("Relaiskarte_0003/OUT/B","off");
-                delay(100);
-              }
-      }      
-
-
-    if (strcmp(topic,"Relaiskarte_0003/IN/C")==0) {
-
-        // Kanal C
-        if ((char)payload[0] == 'o' && (char)payload[1] == 'n') {  
-                 Serial.println("relais_C -> AN");
-                 digitalWrite(relais_C, !HIGH);
-                 client.publish("Relaiskarte_0003/OUT/C","on");                 
-                delay(100);
-              }
-
-        if ((char)payload[0] == 'o' && (char)payload[1] == 'f' && (char)payload[2] == 'f') {  
-                 //digitalWrite(relais_A, !LOW);
-                 Serial.println("relais_C -> AUS");
-                 digitalWrite(relais_C, !LOW);
-                 client.publish("Relaiskarte_0003/OUT/C","off");
-                delay(100);
-              }
-      }
-
-
-    if (strcmp(topic,"Relaiskarte_0003/IN/D")==0) {
-
-        // Kanal D
-        if ((char)payload[0] == 'o' && (char)payload[1] == 'n') {  
-                 Serial.println("relais_D -> AN");
-                 digitalWrite(relais_D, !HIGH);
-                 client.publish("Relaiskarte_0003/OUT/D","on");                 
-                delay(100);
-              }
-
-        if ((char)payload[0] == 'o' && (char)payload[1] == 'f' && (char)payload[2] == 'f') {  
-                 Serial.println("relais_D -> AUS");
-                 digitalWrite(relais_D, !LOW);
-                 client.publish("Relaiskarte_0003/OUT/D","off");
-                delay(100);
-              }
-      }
-
 
 }
 
-
-
+/////////////////////////////////////////////////////////////////////////// Reconnect
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
@@ -121,16 +110,16 @@ void reconnect() {
     Serial.print(mqtt_server);
     Serial.println("");
     // Create a random client ID
-    String clientId = "Haussteuerung-";
+    String clientId = "HolzvergaserSteuerung-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
-      Serial.println("Verbunden ....");
+      Serial.println("MQTT Verbunden ....");
       // ... and resubscribe
-      client.subscribe("Relaiskarte_0003/IN/A");
-      client.subscribe("Relaiskarte_0003/IN/B");
-      client.subscribe("Relaiskarte_0003/IN/C");
-      client.subscribe("Relaiskarte_0003/IN/D");           
+      client.subscribe("Heizung/Holzvergaser/geblaese/IN");
+      client.subscribe("Heizung/Holzvergaser/pumpe/heizkreis/IN");
+
+         
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -141,29 +130,9 @@ void reconnect() {
   }
 }
 
-void setup() {
-
-//Pins deklarieren
-  pinMode(relais_A, OUTPUT);
-  pinMode(relais_B, OUTPUT);
-  pinMode(relais_C, OUTPUT);
-  pinMode(relais_D, OUTPUT);
-
-// Alle Relais auf LOW setzen
-
-  digitalWrite(relais_A, !LOW);
-  digitalWrite(relais_B, !LOW);
-  digitalWrite(relais_C, !LOW);
-  digitalWrite(relais_D, !LOW);
-
-// MQTT Broker
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-
-// Serielle Kommunikation starten
-  Serial.begin(115200);
-
-  // Verbindung zum WiFI aufbauen
+/////////////////////////////////////////////////////////////////////////// SETUP - Wifi
+void wifi_setup() {
+// Verbindung zum WiFI aufbauen
 
   Serial.print("Verbindung zu SSID -> ");
   Serial.println(ssid);
@@ -180,28 +149,105 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.print("ID der Relaiskarte: ");
   Serial.println(kartenID);
-
-// Start mit Relais klack bestaetigen
-
-digitalWrite(relais_A, !LOW);
-delay(300);
-digitalWrite(relais_A, !HIGH);
-delay(300);
-digitalWrite(relais_A, !LOW);
-delay(300);
-digitalWrite(relais_A, !HIGH);
-delay(300);
-digitalWrite(relais_A, !LOW);
 }
 
+/////////////////////////////////////////////////////////////////////////// SETUP
+void setup() {
 
-void loop() {
+//*********************************************** MQTT Broker
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
+//*********************************************** Serielle Kommunikation starten
+  Serial.begin(115200);
+
+//*********************************************** Wifi Setup
+wifi_setup();
+
+//*********************************************** pcf8574
+  pcf8574.pinMode(P0, OUTPUT);
+  pcf8574.pinMode(P1, OUTPUT);
+  pcf8574.pinMode(P2, OUTPUT);
+  pcf8574.pinMode(P3, OUTPUT); 
+  pcf8574.pinMode(P4, OUTPUT); 
+  pcf8574.pinMode(P5, OUTPUT); 
+  pcf8574.pinMode(P6, OUTPUT); 
+  pcf8574.pinMode(P7, OUTPUT);     
+  pcf8574.begin();
+
+  //*********************************************** Relais OFF
+pcf8574.digitalWrite(P0, !LOW);
+pcf8574.digitalWrite(P1, !LOW);
+pcf8574.digitalWrite(P2, !LOW);
+pcf8574.digitalWrite(P3, !LOW);
+pcf8574.digitalWrite(P4, !LOW);
+pcf8574.digitalWrite(P5, !LOW);
+pcf8574.digitalWrite(P6, !LOW);
+pcf8574.digitalWrite(P7, !LOW);
+
+}
+
+/////////////////////////////////////////////////////////////////////////// Temperatur auslesen
+void temp_messen() {
+
+  // Kessel Temp 
+/*
+  int currentTemp2 = sensors.getTempC(temp_kollektor);
+  dtostrf(currentTemp2, 4, 2, stgFromFloat);
+
+   if ((currentTemp2 == -127)||(currentTemp2 == 85))  { 
+     } 
+    else 
+        {     
+        
+        sprintf(msgToPublish, "%s", stgFromFloat);
+        sprintf(textTOtopic, "%s", topic_kollektor);
+        client.publish(textTOtopic, msgToPublish);
+        }
+*/
+  sprintf(textTOtopic, "%s", topic_temp_abgas);
+  client.publish(textTOtopic, "11");
+
+  // Vorlauf Temp
+  sprintf(textTOtopic, "%s", topic_temp_kessel);
+  client.publish(textTOtopic, "22");
+  // Rücklauf Temp
+  sprintf(textTOtopic, "%s", topic_temp_rl);
+  client.publish(textTOtopic, "33");
+  // Abgas Temp
+  sprintf(textTOtopic, "%s", topic_temp_vl);
+  client.publish(textTOtopic, "44");
+}
+
+/////////////////////////////////////////////////////////////////////////// mqtt Callback aufrufen
+void mqtt_callback_aufrufen() {
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
+}
+
+/////////////////////////////////////////////////////////////////////////// LOOP
+void loop() {
 
 
-  
+
+   //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ mqtt Callback aufrufen
+  if (millis() - previousMillis_mqtt_callback> interval_mqtt_callback) {
+      previousMillis_mqtt_callback= millis(); 
+      // Prüfen der Panelenspannung
+      Serial.println("mqtt Callback aufrufen");
+      mqtt_callback_aufrufen();
+    } 
+
+
+  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Temperatur messen
+  if (millis() - previousMillis_temp_messen > interval_temp_messen) {
+      previousMillis_temp_messen= millis(); 
+      // Prüfen der Panelenspannung
+      Serial.println("Temperatur messen");
+      temp_messen();
+    }
+
+ 
 }
